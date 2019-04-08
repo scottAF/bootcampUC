94b75fb0547aef112836bd421cf2ccce5001388f
Scott Ford <uofoduck14@gmail.com>
2019-04-08 21:50:39 +0000

    Error fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
-//= require jquery3
 
