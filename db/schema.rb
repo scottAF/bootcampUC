4d19e416bcf0a45530f7392ae21704edd4b4e4fd
Scott Ford <uofoduck14@gmail.com>
2019-08-29 17:05:35 +0000

    Updated gems. Brought app back to working.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_034855) do
+ActiveRecord::Schema.define(version: 2019_04_15_215433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_08_034855) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source_url"
   end
 
 end
